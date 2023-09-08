658b1d1e661fc03c521fcbbb374ca339fe3ac615
hikari <runrunrun4649@icloud.comm>
2023-09-08 18:20:16 +0900

    standaloneエラー修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+/* ↓↓↓追加した部分↓↓↓ */
+  output: 'standalone',
+/* ↑↑↑追加した部分↑↑↑ */
 }
 
 module.exports = nextConfig
